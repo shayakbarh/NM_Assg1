46dca05f8be8328ec12ff03ff59e220a37863963
Shayak Barh <shayakbarh10@gmail.com>
2023-11-15 12:24:01 +0530

    Adding initial files

---
+++ dividing_1.py
@@ -0,0 +1,14 @@
+
+# dividing 1 by 0,1,2,3,4,5
+
+for i in range(6):
+    # i is the divisor
+    try:
+        result = 1/i
+        print(f"1/{i} = {result}")
+    except ZeroDivisionError:
+        print(f"1/{i} = indeterminate")
+
+
+ 
+        
