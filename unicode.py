b1959b284e99cacd8e07aad74fde5fa8ddf72fda
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 09:57:52 +0530

    Print each character of a the string 'mumbai' and its Unicode code point

---
+++ unicode.py
@@ -0,0 +1,5 @@
+# Python code that prints each character of the string "mumbai" and its Unicode code point
+S = 'mumbai'
+
+for char in S:
+    print(f"Character: {char}, Unicode Code Point: {ord(char)}")
