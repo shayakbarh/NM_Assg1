8039e58a1caf743bcace010b2d91eb8a35e40cc8
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 10:28:33 +0530

    Compute the sum of these Unicode code points and print it

--- unicode.py
+++ unicode.py
@@ -1,5 +1,16 @@
-# Python code that prints each character of the string "mumbai" and its Unicode code point
+"""
+    Python code that prints each character of the string "mumbai" 
+    and its Unicode code point and sum of these Unicode code points    
+"""
+
 S = 'mumbai'
+
+unicode_sum = 0
 
 for char in S:
     print(f"Character: {char}, Unicode Code Point: {ord(char)}")
+
+    unicode_sum += ord(char)
+    
+
+print(f"\nSum of Unicode Code Points: {unicode_sum}")
