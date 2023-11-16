7ec6f72930574de5fd4e66411386cb577e5ba33e
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 11:47:37 +0530

    Eliminating the found flag and final if statement by while-else construction

--- power.py
+++ power.py
@@ -1,19 +1,17 @@
 # Python code that searches a list of numbers for 2^5
 
+# Now eliminate the found flag and the final if statement by using the Python's while else construction
+
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-found = False
 i = 0
-while not found and i < len(L):
-    if 2 ** X == L[i]:
-        found = True
-    else:
-        i = i+1
-    
 
-if found:
-    print('at index', i)
+while i < len(L):
+    if 2 ** X == L[i]:
+        print('at index', i)
+        break
+    i += 1
 else:
     print(X, 'not found')
-    
+
