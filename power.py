904723fc816eba030663848f1231c251ef57e122
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 12:22:37 +0530

    Using Python's for-else and list index method

--- power.py
+++ power.py
@@ -1,17 +1,16 @@
 # Python code that searches a list of numbers for 2^5
 
-# Now eliminate the found flag and the final if statement by using the Python's while else construction
+# Now using Python's for-else construction and list index method to eliminate the explicit list-indexing logic
 
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-i = 0
-
-while i < len(L):
-    if 2 ** X == L[i]:
+for i, value in enumerate(L):
+    if 2 ** X == value:
         print('at index', i)
         break
-    i += 1
 else:
     print(X, 'not found')
+
+
 
