abb0b0fe980edb916885a76e9a46463ba323a32b
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 22:24:32 +0530

    Using print statement to see the results

--- addition.py
+++ addition.py
@@ -9,3 +9,10 @@ def adder(x,y):
 result_string = adder("Navy", "Nagar")
 result_list = adder([1, 2, 3], [4, 5, 6])
 result_float = adder(2.12, 4.57)
+
+# Using print statement to see the results
+print("\nAddition of two strings: ", result_string)
+
+print("\nAddition of two lists: ", result_list)
+
+print("\nAddition of two floats: ", result_float)
