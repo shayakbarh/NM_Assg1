c19f8a7df6d2ec66d294aa52a8de9089915b41a8
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 11:07:36 +0530

    Create list using list comprehension

--- unicode.py
+++ unicode.py
@@ -1,24 +1,26 @@
 """
     Python code to print each character of the string "mumbai" 
     and its Unicode code point and sum of these Unicode code points
-    and to create a list that contains the Unicode code points of the characters in S using list methods   
+    and to create a list that contains the Unicode code points of the characters in S using list comprehension  
 """
 
 S = 'mumbai'
 
 unicode_sum = 0
 
-unicode_list_method = []
 
 for char in S:
     print(f"Character: {char}, Unicode Code Point: {ord(char)}")
-
-    # create a list using list method
-    unicode_list_method.append(ord(char))
 
     unicode_sum += ord(char)
     
 
 print(f"\nSum of Unicode Code Points: {unicode_sum}")
 
-print("\nUsing List Methods:", unicode_list_method)
+
+# create a list using list comprehension
+
+unicode_list_comprehension = [ord(char) for char in S]
+print("\nUsing List Comprehension:", unicode_list_comprehension)
+
+
