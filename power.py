3c4535e711b9c3ea5582e349a005bd26fc1935c8
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 12:34:38 +0530

    Removing loop completely by using in operator

--- power.py
+++ power.py
@@ -1,16 +1,11 @@
 # Python code that searches a list of numbers for 2^5
 
-# Now using Python's for-else construction and list index method to eliminate the explicit list-indexing logic
+# Now removing the loop completely by using the 'in' operator
 
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-for i, value in enumerate(L):
-    if 2 ** X == value:
-        print('at index', i)
-        break
+if 2 ** X in L:
+    print(f"at index {L.index(2 ** X)}")
 else:
-    print(X, 'not found')
-
-
-
+    print(f"{X} not found")
