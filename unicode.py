9c2ab1b342d2532f401bc243c75e492756e9b07a
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 10:56:47 +0530

    Create a list that contains Unicode code points using list methods

--- unicode.py
+++ unicode.py
@@ -1,16 +1,24 @@
 """
-    Python code that prints each character of the string "mumbai" 
-    and its Unicode code point and sum of these Unicode code points    
+    Python code to print each character of the string "mumbai" 
+    and its Unicode code point and sum of these Unicode code points
+    and to create a list that contains the Unicode code points of the characters in S using list methods   
 """
 
 S = 'mumbai'
 
 unicode_sum = 0
 
+unicode_list_method = []
+
 for char in S:
     print(f"Character: {char}, Unicode Code Point: {ord(char)}")
+
+    # create a list using list method
+    unicode_list_method.append(ord(char))
 
     unicode_sum += ord(char)
     
 
 print(f"\nSum of Unicode Code Points: {unicode_sum}")
+
+print("\nUsing List Methods:", unicode_list_method)
