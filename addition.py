8d13163c86bc933764d7863a7486006328152ac4
Shayak Barh <shayakbarh10@gmail.com>
2023-11-16 22:17:57 +0530

    Calling the adder function with different kind of arguments

--- addition.py
+++ addition.py
@@ -4,3 +4,8 @@
 def adder(x,y):
     return x + y
 
+# Calling the adder function with different types of arguments
+
+result_string = adder("Navy", "Nagar")
+result_list = adder([1, 2, 3], [4, 5, 6])
+result_float = adder(2.12, 4.57)
