628da9dc31cd7d15c913c77749b4b6b8d9518c69
Shayak Barh <shayakbarh10@gmail.com>
2023-11-17 12:14:13 +0530

    factorial calculation using standard python

---
+++ factorial_stdpython.py
@@ -0,0 +1,16 @@
+# Factorial calculation using standard python 
+# Here I use recursion method while definig factorial function  
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+n=3    # we want to calculate factorial of 3 
+print(f"Factorial of {n} is {factorial(n)}")
+
+
+
+
+
