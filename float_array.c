0582ff11c3de11a1e55992b37d17761ea91fef2e
Shayak Barh <shayakbarh10@gmail.com>
2023-11-17 09:54:36 +0530

    Creating a function that takes  an array of arbitrary length and returns an array of two elements, 1st and 2nd element are the mean and variance of the input array respectively

--- float_array.c
+++ float_array.c
@@ -1,15 +1,19 @@
 /*
-    Using the malloc standard library function to create an array of hundred float elements 
-    and assigning the values 1^2,2^2,......,100^2 to the elements of this array.
-
+    Creating a function that takes in an array of arbitrary length and returns an array of two elements, 
+    first one is mean and second one is variance.
 */ 
+
 
 #include<stdio.h>
 #include<stdlib.h>
 
+
+void mean_variance(float *array, int length, float *relust);
+
 int main()
 {  
     // Using malloc to allocate memory for an array of 100 floats
+
     float*array;
     array=(float*)malloc(100*sizeof(float));
 
@@ -18,19 +22,37 @@ int main()
         array[i]=(i+1)*(i+1);
     }
 
-    
-    // Printing the array elements
-    printf("Array elements: ");
-    for(int i=0; i<100; i++)
-    {
-        printf("%.2f ", array[i]);
-        
-    }
-
-    printf("\n");
-
-
-
     return 0; // Return 0 to indicate successful execution
 
+}
+
+void mean_variance(float *given_array, int length, float *relust)
+{
+    float mean = 0.0;
+    float variance = 0.0;
+
+
+    // calculating mean value 
+
+    for( int i=0; i<length; i++)
+    {
+        mean += given_array[i];
+    }
+    mean /=length;
+
+
+    // calculating varriance of array 
+
+    for( int i=0; i<length; i++)
+    {
+        variance += (given_array[i] - mean)*(given_array[i] - mean);
+    }
+    variance /= length;
+
+
+    // assigning the result to the output array 
+
+    relust[0]=mean;
+    relust[1]=variance;
+    
 }
