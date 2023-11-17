1c356419ef35394de5e0630701d75e3d09d9378e
Shayak Barh <shayakbarh10@gmail.com>
2023-11-17 13:01:14 +0530

    finding execution time of factorial calculation using Python but writing factorial function in C

--- factorial_C_PY.py
+++ factorial_C_PY.py
@@ -1,15 +1,15 @@
-# Factorial calculation Using Python, but writing the factorial function in C
+# Factorial calculation Using Python, but writing the factorial function in C and finding its execution time
 
 import ctypes
+import os
 
-# Load the C library
-factorial_lib = ctypes.CDLL('./factorial.so')  # Replace with the actual path to the compiled shared object file
+# Determine the correct file extension based on the operating system
+library_extension = '.dll' if os.name == 'nt' else '.so'
 
-# Define the Python wrapper for the factorial function
-def factorial(n):
-    return factorial_lib.factorial(n)
+# Load the shared library
+factorial_lib = ctypes.CDLL(f'./factorial{library_extension}')
 
-# Example usage
-n = 6
-result = factorial(n)
+# Call the C function from Python
+n = 5
+result = factorial_lib.factorial(n)
 print(f"Factorial of {n} is {result}")
