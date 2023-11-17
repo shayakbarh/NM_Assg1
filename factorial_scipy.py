73102b1845e221cd354f2d0df5a6167222b4f5eb
Shayak Barh <shayakbarh10@gmail.com>
2023-11-17 12:58:01 +0530

    finding execution time of factorial calculation using SciPy

--- factorial_scipy.py
+++ factorial_scipy.py
@@ -1,6 +1,15 @@
-# Factorial calculation Using Python, but with the factorial function from SciPy 
+# Factorial calculation Using Python, but with the factorial function from SciPy and finding its execution time
 
 from scipy.special import factorial
-n = 5   # here I want to calculate factorial of 5
-result = factorial(n)
-print(f"Factorial of {n} is {result}")
+import timeit
+from scipy.special import factorial
+import timeit
+# Function to calculate factorial using SciPy
+def calculate_factorial(n):
+    return factorial(n)
+if __name__ == "__main__":
+    n = 5 # Example: Calculate factorial of 5
+    elapsed_time = timeit.timeit(lambda: calculate_factorial(n), number=100000)
+    result = calculate_factorial(n)
+    print(f"Factorial of {n} is {result}")
+    print(f"Time taken: {elapsed_time:.10f} seconds")
