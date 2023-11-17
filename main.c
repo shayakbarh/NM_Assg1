a81e7a219528823a2f8d43530ea528905a2098e5
Shayak Barh <shayakbarh10@gmail.com>
2023-11-17 11:26:35 +0530

    using calloc instead of malloc

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
-// Here I moved my function to a second file named stat.c
-
+// main.c
 #include <stdio.h>
 #include <stdlib.h>
 #include "stats.h"
 
 int main() {
    float*my_array;
-    my_array=(float*)malloc(100*sizeof(float));
+    my_array=(float*)calloc(100, sizeof(float));
 
     for (int i = 1; i < 101; i++)
     {
@@ -22,19 +21,10 @@ int main() {
     printf("Mean: %f\n", result[0]);
     printf("Variance: %f\n", result[1]);
 
-    // Write the result to a text file
-    FILE *textFile = fopen("result.txt", "w");
-    fprintf(textFile, "Mean: %f\n", result[0]);
-    fprintf(textFile, "Variance: %f\n", result[1]);
-    fclose(textFile);
-
-    // Write the result to a binary file
-    FILE *binaryFile = fopen("result.bin", "wb");
-    fwrite(result, sizeof(float), 2, binaryFile);
-    fclose(binaryFile);
+    
 
      // Free the allocated memory
     free(my_array);
     return 0;
-
 }
+
